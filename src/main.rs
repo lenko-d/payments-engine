8a57c90ec67071e8925b2e249ffb162c31bc0f39
Lenko Donchev <lenko.donchev@gmail.com>
2025-05-18 17:06:21 -0500

    Add check for missing command line argument.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ use crate::account::Account;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: \n payments-engine <transactions.csv>");
+        return;
+    }
+
     let transactions_file = &args[1];
     
     let accounts = transactions::from_file(transactions_file);
